6a80b015fdf87a10701b311e50fae21a68336b3f
Kevin Tucker <Kevin.Tucker19@gmail.com>
2020-10-19 15:08:52 -0400

    mid-day commit

---
+++ end_of_lesson_exercises/small_exercises/wk2_exercise1.py
@@ -0,0 +1,5 @@
+numbers = [6,7,8,4,878,312,109]
+num = 0
+for k in numbers:
+   num += k
+print(num)
---
+++ end_of_lesson_exercises/small_exercises/wk2_exercise2.py
@@ -0,0 +1,10 @@
+
+numbers = [6,7,8,4,312,109,878]
+
+for num in numbers:
+    biggest = 0
+    if biggest < num:
+        biggest = num
+    else:
+        pass
+print(biggest)
