6def579e57c6885bf067102e95855ceea0aa282d
Kevin Tucker <Kevin.Tucker19@gmail.com>
2020-11-23 10:14:23 -0500

    updating repo

---
+++ advanced_ui/day3/ajax.js
@@ -0,0 +1,33 @@
+let me = `{
+    "name": "Kevin",
+    "age": 30,
+    "title": "Demi-God Slayer"
+}`
+let objme = JSON.parse(me)
+console.log(objme)
+
+let family = `[
+    {
+        "name": "Kevin",
+        "age": 30,
+        "title": "Demi-God Slayer"
+    },
+    {
+        "name": "Jahnette",
+        "age": 28,
+        "title": "Demi-God Slayer"
+    },
+    {
+        "name": "Tezrah",
+        "age": 3,
+        "title": "Demi-God Slayer"
+    },
+    {
+        "name": "Ivory",
+        "age": 2,
+        "title": "Demi-God Slayer"
+    }
+]`
+
+let objfam = JSON.parse(family)
+console.log(objfam)
